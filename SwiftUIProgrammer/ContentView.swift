b48849291f7911cb55fa6ebfe933a7cd00548a05
thobio <thobio.j@zerone-consulting.com>
2019-12-17 13:35:11 +0530

    Initial Commit

---
+++ SwiftUIProgrammer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIProgrammer
+//
+//  Created by Zerone on 17/12/19.
+//  Copyright © 2019 Zerone. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
